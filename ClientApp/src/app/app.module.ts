b48b1b425cb4f8d540be8c8b0aabbabcfb2f9f9f
George Nairooze <george.nairooze@linkdev.com>
2021-07-12 19:18:54 +0200

    fix client app

---
+++ ClientApp/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MemberComponent } from './member/member.component';
+import { MemberFormComponent } from './member/member-form/member-form.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MemberComponent,
+    MemberFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
