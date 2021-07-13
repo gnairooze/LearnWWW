a7f29c19cf6bc258739cf9e2a8941660b6c873b9
George Nairooze <george.nairooze@linkdev.com>
2021-07-13 13:45:59 +0200

    enhance reading config

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -10,31 +10,9 @@ import { AppComponent } from './app.component';
 import { MemberComponent } from './member/member.component';
 import { MemberFormComponent } from './member/member-form/member-form.component';
 import { ConfigService } from './shared/config.service';
-import { Observable, ObservableInput, of } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
 
 function load(http: HttpClient, config: ConfigService): (() => Promise<boolean>) {
-  return (): Promise<boolean> => {
-    return new Promise<boolean>((resolve: (a: boolean) => void): void => {
-      http.get('../assets/config.json')
-        .pipe(
-          map((x: any) => {
-            config.baseUrl = x.baseUrl;
-            console.log("read base url from config | " + config.baseUrl);
-            resolve(true);
-          }),
-          catchError((x: { status: number }, caught: Observable<void>): ObservableInput<{}> => {
-            if (x.status !== 404) {
-              resolve(false);
-            }
-            config.baseUrl = 'https://localhost:44362/api/member';
-            console.log("set default base url " + config.baseUrl);
-            resolve(true);
-            return of({});
-          })
-        ).subscribe();
-    });
-  };
+  return config.load()
 }
 @NgModule({
   declarations: [
--- ClientApp/src/app/shared/config.service.ts
+++ ClientApp/src/app/shared/config.service.ts
@@ -1,9 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable, ObservableInput, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
+  readonly configUrl = 'assets/config.json';
+  //start declaration of all config paramters
   baseUrl: string = '';
-  constructor() { }
+  //end declaration of all config paramters
+  constructor(private http: HttpClient) { }
+
+  load(): (() => Promise<boolean>) {
+    return (): Promise<boolean> => {
+      return new Promise<boolean>((resolve: (a: boolean) => void): void => {
+        this.http.get(this.configUrl)
+          .pipe(
+            map((x: any) => {
+              //start setting the parameters from config file
+              this.baseUrl = x.baseUrl;
+              //end setting the parameters from config file
+              console.log("read base url from config | " + this.baseUrl);
+              resolve(true);
+            }),
+            catchError((x: { status: number }, caught: Observable<void>): ObservableInput<{}> => {
+              if (x.status !== 404) {
+                resolve(false);
+              }
+              //start setting default values of the parameters
+              this.baseUrl = 'https://localhost:44362/api/member';
+              //end setting default values of the parameters
+              console.log("set default base url " + this.baseUrl);
+              resolve(true);
+              return of({});
+            })
+          ).subscribe();
+      });
+    };
+  }
 }
